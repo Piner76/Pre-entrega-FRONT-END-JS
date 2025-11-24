c0cff7d6a9fc198107e6d42e49f965a9b8bafb50
Piner76 <cabralcristian@gmail.com>
2025-11-24 16:53:54 -0300

    actualizo imagenes de productos a jpg

--- js/arrayDatos.js
+++ js/arrayDatos.js
@@ -4,7 +4,7 @@
  */
 
 export const arrayCategorias = [
-    {nombre : 'Cervezas',img:"../img/Cervesas2.jpgavif", descripcion : 'Las mejores cervezas nacionales e importadas.'},
+    {nombre : 'Cervezas',img:"../img/Cervesas2.jpg", descripcion : 'Las mejores cervezas nacionales e importadas.'},
     {nombre : 'Vinos', img:"../img/vinos.jpg", descripcion : 'Variedades tintos, blancos y espumantes.'},    
     {nombre : 'Bebidas',img:"../img/tragos2.jpg", descripcion : 'Refrescos clasicos y nuevas tendencias'}
 ]
